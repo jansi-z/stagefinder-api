5dfdab509edd4f60a029ead9299917a4841d76e2
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-18 14:39:39 +0200

    No authorisation needed to fetch events

--- src/models/events.model.js
+++ src/models/events.model.js
@@ -6,15 +6,19 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const events = new Schema({
+
     title: { type: String, required: true },
     description: { type: String, required: true },
+    photo: { type: String },
+
     time: { type: String, required: true },
     date: { type: Date, required: true },
     paid: { type: Number, default: null },
     artistCount: { type: Number, required: true },
-    photo: { type: String },
+
     artistIds: [{ type: Schema.Types.ObjectId, ref: 'artists'}],
     venueId: { type: Schema.Types.ObjectId, ref: 'venues' },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
