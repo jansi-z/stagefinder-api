cb9d018495fb8adb882f3870ccef377f3e5cd893
jobweeda <jobweeda@hotmail.com>
2017-10-18 11:31:36 +0200

    if an artist joins an event he is added to the event and vice versa

--- src/services/artists/artists.hooks.js
+++ src/services/artists/artists.hooks.js
@@ -4,14 +4,16 @@ const addArtistToUser = require('../../hooks/add-artist-to-user');
 
 const addUserToArtistProfile = require('../../hooks/add-user-to-artist-profile');
 
+const updateArtist = require('../../hooks/update-artist');
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
     create: [authenticate('jwt'), addUserToArtistProfile()],
-    update: [authenticate('jwt')],
-    patch: [authenticate('jwt')],
+    update: [authenticate('jwt'), updateArtist()],
+    patch: [authenticate('jwt'), updateArtist()],
     remove: [authenticate('jwt')]
   },
 
